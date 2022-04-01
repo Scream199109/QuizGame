f2de84c742ba484b6a17079f7dba090d67cebee6
Scream199109 <Scream199109@mail.ru>
2022-04-01 09:59:20 +0300

    slider

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ config(app);
 app.use('/', mainRoute);
 // app.use('/reg', registrationRoute);
 
+app.get('/slider', (req, res) => {
+  res.render('slider')
+})
+app.get('/card', (req, res) => {
+  res.render('card')
+})
+
 app.listen(port, () => {
   console.log(`Server started at ${port} port`);
 });
